397a6cf34d6e7ac2dd7d3e6145e8dbaf164b168d
eriXtrip <150985893+eriXtrip@users.noreply.github.com>
2024-02-20 09:45:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -27,39 +27,39 @@ themeButton.addEventListener('click', () => {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    // document.getElementById("button__proj").addEventListener("click", function(event) {
-    //     event.preventDefault();
-    //     window.open("https://erixtrip.github.io/upDatedPortfolio/index.html#Proj", "_blank");
-    // });
-
+    document.getElementById("button__proj").addEventListener("click", function(event) {
+        event.preventDefault();
+        window.open("/index.html#Proj", "_blank");
+    });
+    
     document.getElementById("education__link").addEventListener("click", function(event) {
         event.preventDefault();
-        window.open("file:///C:/Users/Tripulca/Desktop/WEB%20SYSTEMS%20AND%20TECHNOLOGIES/index.html#Skill__Ed", "_blank");
+        window.open("/index.html#Skill__Ed", "_blank");
     });
-
+    
     document.getElementById("skills__link").addEventListener("click", function(event) {
         event.preventDefault();
-        window.open("file:///C:/Users/Tripulca/Desktop/WEB%20SYSTEMS%20AND%20TECHNOLOGIES/index.html#Skill__Ed", "_blank");
+        window.open("/index.html#Skill__Ed", "_blank");
     });
-
+    
     document.getElementById("PersonalINF__link").addEventListener("click", function(event) {
         event.preventDefault();
-        window.open("file:///C:/Users/Tripulca/Desktop/WEB%20SYSTEMS%20AND%20TECHNOLOGIES/index.html#PersonalInformation", "_blank");
+        window.open("/index.html#PersonalInformation", "_blank");
     });
-
+    
     document.getElementById("moreskill__link").addEventListener("click", function(event) {
         event.preventDefault();
-        window.open("file:///C:/Users/Tripulca/Desktop/WEB%20SYSTEMS%20AND%20TECHNOLOGIES/index.html#otherSkill", "_blank");
+        window.open("/index.html#otherSkill", "_blank");
     });
-
+    
     document.getElementById("hobbies__link").addEventListener("click", function(event) {
         event.preventDefault();
-        window.open("file:///C:/Users/Tripulca/Desktop/WEB%20SYSTEMS%20AND%20TECHNOLOGIES/index.html#hobbies", "_blank");
+        window.open("/index.html#hobbies", "_blank");
     });
-
+    
     document.getElementById("relationship__link").addEventListener("click", function(event) {
         event.preventDefault();
-        window.open("file:///C:/Users/Tripulca/Desktop/WEB%20SYSTEMS%20AND%20TECHNOLOGIES/index.html#relationship", "_blank");
+        window.open("/index.html#relationship", "_blank");
     });
 
     document.getElementById("more").addEventListener("click", function(event) {
